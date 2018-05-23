8184cd528b971e288b0a80c9f8d15c55dca44d8e
Manny Ramaswamy <manish.ram@gmail.com>
2018-05-22 21:40:57 -0400

    Updated steer to steering and modified PID gains

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -3,26 +3,26 @@ from lowpass import LowPassFilter
 from yaw_controller import YawController
 import rospy
 GAS_DENSITY = 2.858
-ONE_MPH = 0.44704
+ONE_MPH = 1
 FULL_BRAKE_SPD = 0.1
 
 
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
-        accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+                 accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
 
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, ONE_MPH, max_lat_accel, max_steer_angle)
 
-        kp = 0.3
-        ki = 0.1
+        kp = 1.5
+        ki = 0.001
         kd = 0.
-        mn = 0. # Minimum throttle value
-        mx = 0.8 # Maximum throttle value
+        mn = 0.  # Minimum throttle value
+        mx = 0.8  # Maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
-        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
-        ts = 0.02 # Sample time
+        tau = 4.  # 1/(2pi*tau) = cutoff frequency
+        ts = 1.  # Sample time
         self.vel_lpf = LowPassFilter(tau, ts)
 
         self.vehicle_mass = vehicle_mass
@@ -50,23 +50,22 @@ class Controller(object):
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
 
-
         current_time = rospy.get_time()
         sample_time = current_time - self.last_time
         self.last_time = current_time
 
         linear_accel = self.throttle_controller.step(vel_error, sample_time)
 
-         # Full brake if target velocity is almost 0
+        # Full brake if target velocity is almost 0
         if linear_vel < FULL_BRAKE_SPD:
             throttle = 0.0
             brake = self.acceleration_to_torque(abs(self.decel_limit))
         else:
-            if linear_accel > 0.0: # Tipin - Vehicle moving/accelerating
+            if linear_accel > 0.0:  # Tipin - Vehicle moving/accelerating
                 throttle = linear_accel
                 brake = 0.0
             else:
-                throttle = 0.0 # Tipout - vehicle slowing down
+                throttle = 0.0  # Tipout - vehicle slowing down
                 decel = -linear_accel
 
                 # For small decel, do not brake
@@ -76,7 +75,7 @@ class Controller(object):
                 # Compute brake torque, in Nm
                 brake = self.acceleration_to_tq(decel)
 
-        return throttle, brake, steer
+        return throttle, brake, steering
 
     def acceleration_to_tq(self, acceleration):
         """
@@ -85,6 +84,3 @@ class Controller(object):
         Output: torque (float) , in Nm
         """
         return acceleration * self.total_mass * self.wheel_radius
-
-
-
